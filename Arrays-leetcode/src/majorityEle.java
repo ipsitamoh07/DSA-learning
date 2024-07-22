47d3163aa16b9d2b43c4dc7c0aa6bae5241c87d7
ipsitamoh07 <138458933+ipsitamoh07@users.noreply.github.com>
2024-07-23 01:47:18 +0530

    medium ques

---
+++ Arrays-leetcode/src/majorityEle.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/majority-element/description/
+
+import java.util.Arrays;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n= nums.length;
+        return nums[n/2];
+    }
+}
